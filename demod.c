681bb6e68187618f5565fa930509af1cfec4be06
casandro <christian@clarke-3.d>
2022-03-20 13:14:33 +0100

    Small program to synchroniously demod QI-samples into AM

---
+++ demod.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define SRATE 2000000.0
+
+#define BLEN (500000)
+
+#define P_FILTER (1e-2)
+
+typedef struct {
+	int16_t i;
+	int16_t q;
+} sample_t;
+
+int main(int argc, char *argv[])
+{
+	int n=0;
+	double phi=0;
+	double frq=-52.25e+3;
+	double t=0;
+
+	double min=99999;
+	double max=-min;
+	double phase_error=0;
+	double ferror=0;
+	
+	sample_t ibuf[BLEN];
+
+	if (argc<3) return 0;
+
+	double a=0;
+	double b=-5;
+
+	while (fread(ibuf, sizeof(ibuf), 1, stdin)>0) {	
+		double obuf[BLEN];
+		for (n=0; n<BLEN; n++) {
+			double i=ibuf[n].i/32768.0;
+			double q=ibuf[n].q/32768.0;
+
+			phi=phi+frq*M_PI*2/SRATE;
+			double i_=cos(phi)*i-sin(phi)*q;
+			double q_=sin(phi)*i+cos(phi)*q;
+			
+			phase_error=phase_error*(1-P_FILTER)+q_*P_FILTER;
+			frq=frq+phase_error*a;
+			phi=phi+phase_error*b;
+			obuf[n]=i_;	
+		}
+		fwrite(obuf, sizeof(obuf), 1, stdout);
+	}
+
+	return 0;
+}
+
